d1aa0cf039af9b949af0abd456630127aa8529da
LORD_FIL_KEN 979 <108487784+K-DEV10@users.noreply.github.com>
2022-08-24 18:44:16 +0000

    BETTER CSS

--- src/App.vue
+++ src/App.vue
@@ -9,34 +9,59 @@
    <div class = "down">
      <a href="src/app.html">
        <button class = "j">
-         Going
+         Going .
          
        </button>
      </a>
    </div>
   <div class = "left">
-    <select>
+    <select class = "select1" >
       <option value ="Example">Example</option>
       
     </select>
     
   </div>
+  <link href='https://fonts.googleapis.com/css?family=Bangers' rel='stylesheet'>
 </template>
+
 <!-- CSS -->
 <style>
   
-    
+  
+  .select1{
+    font-family: 'Bangers'
+  } 
   .down {
     text-align: right;
   }
   .j{
-    font-size: 50px;
+    font-size: 45px;
+    border-radius: 50px;
+    font-family: 'Bangers'
   }
   .left {
     text-align: left;
     font-size: 50px;
     
   }
+  .j:hover {
+
+background: #0091ff;
+
+box-shadow: 0 0 5px #0091ff, 0 0 25px
+
+#0091ff, 0 0 50px #0091ff, 0 0 200px #0091ff;
+
+}
+.select1:hover {
+
+background: #ff96ad;
+
+box-shadow: 0 0 5px #ff96ad, 0 0 25px
+
+#ff96ad, 0 0 50px #ff96ad, 0 0 200px #ff96ad;
+
+}
 
  
 </style>
